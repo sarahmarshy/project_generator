69e7350d647ba00e944a13a46ca5b40ccc1f7930
sarahmarshy <sarah.marsh@arm.com>
2015-08-04 16:48:06 -0500

    More comments

--- project_generator/create.py
+++ project_generator/create.py
@@ -6,119 +6,142 @@ import bisect
 from collections import defaultdict
 
 def _determine_tool(files):
-        for file in files:
-            linker_ext = file.split('.')[-1]
-            if "sct" in linker_ext or "lin" in linker_ext:
-                yield (str(file),"uvision")
-            elif "ld" in linker_ext:
-                yield (str(file),"make_gcc_arm")
-            elif "icf" in linker_ext:
-                yield (str(file),"iar_arm")
+    """Yields tuples in the form of (linker file, tool the file links for"""
+    for file in files:
+        linker_ext = file.split('.')[-1]
+        if "sct" in linker_ext or "lin" in linker_ext:
+            yield (str(file),"uvision")
+        elif "ld" in linker_ext:
+            yield (str(file),"make_gcc_arm")
+        elif "icf" in linker_ext:
+            yield (str(file),"iar_arm")
 
 
 def _scan(section, root, directory, extensions):
-        if section == "sources":
-            data_dict = defaultdict(list)
-        else:
-            data_dict = []
-        for dirpath, dirnames, files in os.walk(directory):
-            for filename in files:
-                ext = filename.split('.')[-1]
-                relpath = os.path.relpath(dirpath, root)
-                if ext in extensions:
-                    if section == "sources":
-                        dir = directory.split(os.path.sep)[-1] if dirpath == directory else dirpath.replace(directory,'').split(os.path.sep)[1]
-                        if relpath not in data_dict[dir]:
-                            bisect.insort(data_dict[dir], relpath)
-                    elif section == 'includes':
-                        dirs = relpath.split(os.path.sep)
-                        for i in range(1, len(dirs)+1):
-                            data_dict.append(os.path.sep.join(dirs[:i]))
-                    else:
-                        data_dict.append(os.path.join(relpath, filename))
-        if section == "sources":
-            return dict(data_dict)
-        l = list(set(data_dict))
-        l.sort()
-        return l
+    if section == "sources":
+        data_dict = defaultdict(list)  # sources can have group names, making them a dict
+    else:
+        data_dict = []
+    for dirpath, dirnames, files in os.walk(directory):
+        for filename in files:
+            ext = filename.split('.')[-1]
+            relpath = os.path.relpath(dirpath, root)
+            if ext in extensions: # Check if the files extension is an acceptable one for this section of yaml
+                if section == "sources":
+                    #get the directory one below root
+                    dir = directory.split(os.path.sep)[-1] if dirpath == directory else dirpath.replace(directory,'').split(os.path.sep)[1]
+                    if relpath not in data_dict[dir]:
+                        # Add the path to the file to this dictionary, with dir as group name
+                        bisect.insort(data_dict[dir], relpath)
+                elif section == 'includes':
+                    #get all the folders along the path
+                    dirs = relpath.split(os.path.sep)
+                    for i in range(1, len(dirs)+1):
+                        """ add all combinations of them, because we don't know how they will be referenced in program files
+                            Example - there is a .h file in the path Here\is\include\include.h
+                            We want to catch any possible combo, so add Here, Here\is, and Here\is\include
+
+                        """
+                        data_dict.append(os.path.sep.join(dirs[:i]))
+                else:
+                    data_dict.append(os.path.join(relpath, filename))
+    if section == "sources":
+        return dict(data_dict)
+    l = list(set(data_dict))
+    l.sort()
+    return l
 
 def _generate_file(filename,root,directory,data):
-        logging.debug('Generating yaml file ' + filename)
-        overwrite = False
-        if os.path.isfile(os.path.join(directory, filename)):
-            print("Project file " +filename+  " already exists")
-            while True:
-                answer = raw_input('Should I overwrite it? (Y/n)')
-                try:
-                    overwrite = answer.lower() in ('y', 'yes')
-                    if not overwrite:
-                        logging.critical('Unable to save project file')
-                        return -1
-                    break
-                except ValueError:
-                    continue
-        if overwrite:
-            os.remove(filename)
-        with open(os.path.join(root, filename), 'w+') as f:
-            f.write(yaml.dump(data, default_flow_style=False))
-        p = os.popen('attrib +h ' + filename)
-        p.close()
-        return 0
+    logging.debug('Generating yaml file ' + filename)
+    overwrite = False
+    if os.path.isfile(os.path.join(directory, filename)):
+        print("Project file " +filename+  " already exists")
+        while True:
+            answer = raw_input('Should I overwrite it? (Y/n)')
+            try:
+                overwrite = answer.lower() in ('y', 'yes')
+                if not overwrite:
+                    logging.critical('Unable to save project file')
+                    return -1
+                break
+            except ValueError:
+                continue
+    if overwrite:
+        os.remove(filename)
+    with open(os.path.join(root, filename), 'w+') as f:
+        f.write(yaml.dump(data, default_flow_style=False))
+    p = os.popen('attrib +h ' + filename) # make the file hidden
+    p.close()
+    return 0
 
 
 def create_yaml(root, directory, project_name, board,cpu):
-        common_section = {
-            'linker_file': FILES_EXTENSIONS['linker_file'],
-            'sources': FILES_EXTENSIONS['source_files_c'] + FILES_EXTENSIONS['source_files_cpp'] +
-                        FILES_EXTENSIONS['source_files_s'] + FILES_EXTENSIONS['source_files_obj'],
-            'includes': FILES_EXTENSIONS['includes'],
-            'target': [],
-        }
-        export_dir = os.path.join("generated_projects","{tool}","{project_name}")
-        projects_yaml = {
-            'projects': {
-                project_name: ['.project.yaml']
-            },
-            'settings': {'export_dir': [export_dir]}
-        }
-        project_yaml = {
-            'common': {'macros': [cpu]},
-            'tool_specific': {}
-        }
+    # lay out what the common section a project yaml file will look like
+    # The value mapped to by each key are the file extensions that will help us get valid files for each section
+    common_section = {
+        'linker_file': FILES_EXTENSIONS['linker_file'],
+        'sources': FILES_EXTENSIONS['source_files_c'] + FILES_EXTENSIONS['source_files_cpp'] +
+                   FILES_EXTENSIONS['source_files_s'] + FILES_EXTENSIONS['source_files_obj'],
+        'includes': FILES_EXTENSIONS['includes'],
+        'target': [],
+    }
+    export_dir = os.path.join("generated_projects","{tool}","{project_name}")
 
-        for section in common_section:
-            if len(common_section[section]) > 0:
-                project_yaml['common'][section] = _scan(section, root, directory,common_section[section])
+    # will be written to .projects.yaml
+    projects_yaml = {
+        'projects': {
+            project_name: ['.project.yaml']
+        },
+        'settings': {'export_dir': [export_dir]}
+    }
 
-        project_yaml['common']['target'] = [board]
+    # will be written to .project.yaml
+    project_yaml = {
+        'common': {'macros': [cpu]},
+        'tool_specific': {}
+    }
 
-        tools = _determine_tool(project_yaml['common']['linker_file'])
+    # iterate over the common section defined above
+    for section, extensions in common_section.items():
+        if len(common_section[section]) > 0:
+            # look for files in this directory that have the defined extensions, and add them to our project file
+            project_yaml['common'][section] = _scan(section, root, directory,extensions)
 
-        linkers = {}
-        for (file,tool) in tools:
-            if tool in linkers:
-                linkers[tool].append(file)
-            else:
-                linkers[tool] = [file]
+    project_yaml['common']['target'] = [board] # user passes target in command line
 
-        for tool, linker in linkers.items():
-            if len(linker) > 1:
-                print "\nMultipe linkers found for " + tool + "\n"
-                for i, file in enumerate(linker):
-                    print str(i) + ": " + file
-                answer = raw_input('\nWhich file do you want? ')
-                while int(answer) not in range(0,len(linker)):
-                    answer = raw_input('Answer not in list. Which file do you want? ')
-                project_yaml['tool_specific'][tool] = {'linker_file': [linkers[tool][int(answer)]]}
-            else:
-                project_yaml['tool_specific'][tool] = {'linker_file': linker}
+    """Find what tool the linker works for - if multiple linkers in this dir, project_yaml['common']['linker_file']
+        is a list. Tools is an iterator"""
+    tools = _determine_tool(project_yaml['common']['linker_file'])
 
-        del project_yaml['common']['linker_file']
+    linkers = {}   # dictionary containing all linker files in the project directory
 
-        ret = _generate_file(".projects.yaml", root, directory, projects_yaml)
-        if ret < 0:
-            return -1
-        _generate_file(".project.yaml", root, directory, project_yaml)
+    # exhaust the iterator and add its yielded values to linkers
+    for (file,tool) in tools:
+        if tool in linkers:
+            linkers[tool].append(file)
+        else:
+            linkers[tool] = [file]
+
+    for tool, linker in linkers.items():
+        if len(linker) > 1:
+            # The project directory defined more than one linker for a tool
+            print "\nMultipe linkers found for " + tool + "\n"
+            for i, file in enumerate(linker):
+                print str(i) + ": " + file
+            answer = raw_input('\nWhich file do you want? ')
+            while int(answer) not in range(0,len(linker)):
+                answer = raw_input('Answer not in list. Which file do you want? ')
+            project_yaml['tool_specific'][tool] = {'linker_file': [linkers[tool][int(answer)]]}
+        else:
+            project_yaml['tool_specific'][tool] = {'linker_file': linker}
+
+    # linker file is now in tool_specific section
+    del project_yaml['common']['linker_file']
+
+    ret = _generate_file(".projects.yaml", root, directory, projects_yaml)
+    if ret < 0: # make sure the first file generated correctly
+        return -1
+    _generate_file(".project.yaml", root, directory, project_yaml)
 
 
 
--- project_generator/generate.py
+++ project_generator/generate.py
@@ -13,14 +13,19 @@ class Generator:
 
     def generate(self, name = '', ignore = []):
         if 'projects' in self.projects_dict:
+            # User has chosen to generate a project based on specifically named project
             if name != '':
+                # Check if the name they specified is even in the yaml ile
                 if name not in self.projects_dict['projects'].keys():
                     raise RuntimeError("You specified an invalid project name.")
                 else:
-                    records =self.projects_dict['projects'][name]
+                    # Get the portion of the yaml that is just the project specified
+                    records = self.projects_dict['projects'][name]
+                    # Load the yamls defined in that section
                     project_dicts = load_yaml_records(uniqify(flatten(records)))
+                    # Yield this generated project to be dealt with in command scripts
                     yield Project(project_dicts,self.projects_dict, name, ignore)
-            else:
+            else:  # user hasn't specified, generate all possible projects
                 for name, records in self.projects_dict['projects'].items():
                     project_dicts = load_yaml_records(uniqify(flatten(records)))
                     yield Project(project_dicts,self.projects_dict,name, ignore)
